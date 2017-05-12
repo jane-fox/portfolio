983a5d15b7cd9aff1106b6204ff9acd98c80b907
J Fox <janevfox@protonmail.com>
2017-05-11 17:49:24 -0700

    reducing amount of sass files, since this is a simple theme

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -59,7 +59,8 @@ function wp_website_portfolio_entry_footer() {
 		}
 	}
 
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+	if (false) {
+	//if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 		echo '<span class="comments-link">';
 		/* translators: %s: post title */
 		comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'wp-website-portfolio' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
