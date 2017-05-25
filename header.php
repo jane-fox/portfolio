dbd8cdd6e8c97bb74fcdf385f8eaa70422f8f3aa
J Fox <janevfox@protonmail.com>
2017-05-24 20:10:28 -0700

    Add favicon

--- header.php
+++ header.php
@@ -12,13 +12,16 @@
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png">
 
 <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
 <?php include_once("analytics.php") ?>
 
