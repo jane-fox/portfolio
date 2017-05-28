50d2e623ff42abe6c495f26dfc9a74005faa474a
J Fox <janevfox@protonmail.com>
2017-05-28 00:22:36 -0700

    Adding resume and github to footer

--- footer.php
+++ footer.php
@@ -15,8 +15,19 @@
 <footer id="site-footer" role="contentinfo">
 	<div class="container">
 		<div class="col-sm-6">
-			<!-- <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wp-website-portfolio' ) ); ?>"><?php printf( esc_html__( 'Powered by %s', 'wp-website-portfolio' ), 'WordPress' ); ?></a> -->
+			<!-- <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wp-website-portfolio' ) ); ?>">
+				<?php printf( esc_html__( 'Powered by %s', 'wp-website-portfolio' ), 'WordPress' ); ?>
+			</a> 
 			Powered by Wordpress | Custom theme based on _s
+			-->
+			<a href="<?php echo get_stylesheet_directory_uri(); ?>/resume.html" target="_blank">
+				<span class="fa fa-file-text"></span> Resume
+			</a>
+			<a href="https://github.com/jane-fox" target="_blank">
+				<span class="fa fa-github"></span> Github
+			</a>
+
+			
 		</div>
 
 		<div class="col-sm-6 footer-right text-right">
